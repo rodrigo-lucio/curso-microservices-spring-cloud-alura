add90b1d73fce6a114713645cc3ac4ad4a23b211
Rodrigo Lúcio <luciodigo@gmail.com>
2021-10-21 23:48:14 -0300

    Ajustado pacotes

--- eureka-server/src/main/java/br/com/alura/microservices/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/br/com/alura/microservices/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.alura.micoservices.eureka;
+package br.com.alura.microservices.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
