0e9c641dd30749cb3bc7cb344a23fe3bc14a3117
Rodrigo Lúcio <luciodigo@gmail.com>
2021-10-27 00:14:38 -0300

    Criado novos DTOs para pedido

---
+++ provider/src/main/java/br/com/alura/microservices/provider/dto/BookingDTO.java
@@ -0,0 +1,11 @@
+package br.com.alura.microservices.provider.dto;
+
+import lombok.Data;
+
+@Data
+public class BookingDTO {
+
+	public Integer bookingId;
+	public Integer preparationTime;
+
+}
---
+++ provider/src/main/java/br/com/alura/microservices/provider/dto/OrderItemDTO.java
@@ -0,0 +1,11 @@
+package br.com.alura.microservices.provider.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderItemDTO {
+
+	private long id;	
+	private int amount;
+	
+}
