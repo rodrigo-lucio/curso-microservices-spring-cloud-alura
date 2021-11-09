a825bc81c137ca98de8eff1c59bcda0c561004f2
Rodrigo Lúcio <luciodigo@gmail.com>
2021-11-08 23:45:03 -0300

    Implementado e testado o Circuit Break com Hystrix

--- shop/src/main/java/br/com/alura/microservices/shop/ShopApplication.java
+++ shop/src/main/java/br/com/alura/microservices/shop/ShopApplication.java
@@ -2,6 +2,7 @@ package br.com.alura.microservices.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ShopApplication {
 
 	public static void main(String[] args) {
--- shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
+++ shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
@@ -3,6 +3,8 @@ package br.com.alura.microservices.shop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.microservices.shop.client.ProviderClient;
 import br.com.alura.microservices.shop.dto.BuyDTO;
 import br.com.alura.microservices.shop.dto.OrderInfoDTO;
@@ -17,12 +19,21 @@ public class BuyService {
 	@Autowired
 	private ProviderClient providerClient;
 
+	@HystrixCommand(fallbackMethod = "makeBuyFallback")
 	public Buy makeBuy(BuyDTO buy) {
 		ProviderInfo infoByState = providerClient.getInfoByState(buy.getAdress().getState());
 		log.info("Get provider info by state {}", infoByState.getAdress());
 		
 		OrderInfoDTO order = providerClient.makeOrder(buy.getItems());
 		log.info("Make order id: {}", order.getId());
+		
+		/* Caso queira testar o fallback do Hystrix
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
 		
 		Buy savedBuy = new Buy();
 		savedBuy.setOrderId(order.getId());
@@ -31,6 +42,12 @@ public class BuyService {
 		log.info("Saved buy: {}", savedBuy);
 		log.info("Buy DTO:  {}", buy);
 		
+		return savedBuy;
+	}
+	
+	public Buy makeBuyFallback(BuyDTO buy) {
+		Buy savedBuy = new Buy();
+		savedBuy.setAdress("Chamou o fallback");
 		return savedBuy;
 	}
 
