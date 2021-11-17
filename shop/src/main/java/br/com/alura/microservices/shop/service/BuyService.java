f1a84b53c41020f42554b3eee9a69badcabbb57b
Rodrigo Lúcio <luciodigo@gmail.com>
2021-11-17 00:02:22 -0300

    Salvado dados do voucher na compra

--- shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
+++ shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservices.shop.service;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -7,9 +9,12 @@ import org.springframework.stereotype.Service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservices.shop.client.ProviderClient;
+import br.com.alura.microservices.shop.client.ShippingClient;
 import br.com.alura.microservices.shop.dto.BuyDTO;
+import br.com.alura.microservices.shop.dto.DeliveryInfoDTO;
 import br.com.alura.microservices.shop.dto.OrderInfoDTO;
 import br.com.alura.microservices.shop.dto.ProviderInfo;
+import br.com.alura.microservices.shop.dto.VoucherDTO;
 import br.com.alura.microservices.shop.model.Buy;
 import br.com.alura.microservices.shop.repository.BuyRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +28,9 @@ public class BuyService {
 	
 	@Autowired
 	private BuyRepository buyService;
+	
+	@Autowired
+	private ShippingClient shippingClient;
 
 	//threadPoolKey=caso o makeBuy fique com as 10 threads cheias (padrao do histryx, ele nao bloqueia a execuçcao do get by id
 	@HystrixCommand(fallbackMethod = "getByIdyFallback", threadPoolKey = "makeBuyThreadPool") 
@@ -38,6 +46,16 @@ public class BuyService {
 		OrderInfoDTO order = providerClient.makeOrder(buy.getItems());
 		log.info("Make order id: {}", order.getId());
 		
+		DeliveryInfoDTO delivery = DeliveryInfoDTO.builder()
+									.orderId(order.getId())
+									.deliveryDate(LocalDate.now().plusDays(order.getPreparationTime()))
+									.senderAdress(infoByState.getAdress())
+									.destinationAdress(buy.getAdress().toString())
+									.build();
+
+		VoucherDTO voucher = shippingClient.bookDelivery(delivery);
+		log.info("Book delivery voucher: {}", voucher.getNumber());
+		
 		/* Caso queira testar o fallback do Hystrix
 		try {
 			Thread.sleep(2000);
@@ -51,6 +69,9 @@ public class BuyService {
 		savedBuy.setPreparationTime(order.getPreparationTime());
 		savedBuy.setAdress(infoByState.getAdress());
 		savedBuy.setState(infoByState.getState());
+		savedBuy.setVoucher(voucher.getNumber());
+		savedBuy.setEstimatedDeliveryDate(voucher.getEstimatedDeliveryDate());
+		
 		log.info("Saved buy: {}", savedBuy);
 		log.info("Buy DTO:  {}", buy);
 		
