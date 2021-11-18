413c9b5061606b1e4b1f4ecd95143def5b95742d
Rodrigo Lúcio <luciodigo@gmail.com>
2021-11-18 00:13:09 -0300

    Melhorado o fluxo no processo da compra com controle de status

--- shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
+++ shop/src/main/java/br/com/alura/microservices/shop/service/BuyService.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -16,6 +17,7 @@ import br.com.alura.microservices.shop.dto.OrderInfoDTO;
 import br.com.alura.microservices.shop.dto.ProviderInfo;
 import br.com.alura.microservices.shop.dto.VoucherDTO;
 import br.com.alura.microservices.shop.model.Buy;
+import br.com.alura.microservices.shop.model.EnumBuyStatus;
 import br.com.alura.microservices.shop.repository.BuyRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,7 +29,7 @@ public class BuyService {
 	private ProviderClient providerClient;
 	
 	@Autowired
-	private BuyRepository buyService;
+	private BuyRepository buyRepository;
 	
 	@Autowired
 	private ShippingClient shippingClient;
@@ -35,27 +37,16 @@ public class BuyService {
 	//threadPoolKey=caso o makeBuy fique com as 10 threads cheias (padrao do histryx, ele nao bloqueia a execuçcao do get by id
 	@HystrixCommand(fallbackMethod = "getByIdyFallback", threadPoolKey = "makeBuyThreadPool") 
 	public Buy getById(Long id) {	
-		return buyService.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return buyRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
 	}
 	
+	@Transactional
 	@HystrixCommand(fallbackMethod = "makeBuyFallback", threadPoolKey = "makeBuyThreadPool")
-	public Buy makeBuy(BuyDTO buy) {
-		ProviderInfo infoByState = providerClient.getInfoByState(buy.getAdress().getState());
-		log.info("Get provider info by state {}", infoByState.getAdress());
-		
-		OrderInfoDTO order = providerClient.makeOrder(buy.getItems());
-		log.info("Make order id: {}", order.getId());
-		
-		DeliveryInfoDTO delivery = DeliveryInfoDTO.builder()
-									.orderId(order.getId())
-									.deliveryDate(LocalDate.now().plusDays(order.getPreparationTime()))
-									.senderAdress(infoByState.getAdress())
-									.destinationAdress(buy.getAdress().toString())
-									.build();
-
-		VoucherDTO voucher = shippingClient.bookDelivery(delivery);
-		log.info("Book delivery voucher: {}", voucher.getNumber());
-		
+	public Buy makeBuy(BuyDTO buyInput) {
+		Buy buy = saveBuyReceived(buyInput);
+		buyInput.setId(buy.getId());
+		OrderInfoDTO order = makeOrder(buyInput, buy);
+		bookDelivery(buyInput, buy, order);
 		/* Caso queira testar o fallback do Hystrix
 		try {
 			Thread.sleep(2000);
@@ -63,32 +54,63 @@ public class BuyService {
 			e.printStackTrace();
 		}
 		*/
+		log.info("Saved buy: {}", buy);
+		log.info("Buy DTO:  {}", buyInput);
 		
-		Buy savedBuy = new Buy();
-		savedBuy.setOrderId(order.getId());
-		savedBuy.setPreparationTime(order.getPreparationTime());
-		savedBuy.setAdress(infoByState.getAdress());
-		savedBuy.setState(infoByState.getState());
-		savedBuy.setVoucher(voucher.getNumber());
-		savedBuy.setEstimatedDeliveryDate(voucher.getEstimatedDeliveryDate());
-		
-		log.info("Saved buy: {}", savedBuy);
-		log.info("Buy DTO:  {}", buy);
-		
-		return buyService.save(savedBuy);
+		return buy;
+	}
+
+	private Buy saveBuyReceived(BuyDTO buyInput) {
+		Buy buy = new Buy();
+		buy.setStatus(EnumBuyStatus.RECEIVED);
+		buy.setRoad(buyInput.getAdress().getRoad());
+		buy.setState(buyInput.getAdress().getState());
+		buy.setNumber(buyInput.getAdress().getNumber());
+		buyRepository.save(buy);
+		return buy;
+	}
+	
+	private OrderInfoDTO makeOrder(BuyDTO buyInput, Buy buy) {
+		OrderInfoDTO order = providerClient.makeOrder(buyInput.getItems());
+		log.info("Make order id: {}", order.getId());
+		buy.setOrderId(order.getId());
+		buy.setPreparationTime(order.getPreparationTime());
+		buy.setStatus(EnumBuyStatus.ORDER_RECEIVED);
+		buyRepository.save(buy);
+		return order;
+	}
+	
+	private void bookDelivery(BuyDTO buyInput, Buy buy, OrderInfoDTO order) {
+		ProviderInfo infoByState = providerClient.getInfoByState(buyInput.getAdress().getState());
+		log.info("Get provider info by state {}", infoByState.getAdress());
+		DeliveryInfoDTO delivery = DeliveryInfoDTO.builder()
+									.orderId(order.getId())
+									.deliveryDate(LocalDate.now().plusDays(order.getPreparationTime()))
+									.senderAdress(infoByState.getAdress())
+									.destinationAdress(buyInput.getAdress().toString())
+									.build();
+
+		VoucherDTO voucher = shippingClient.bookDelivery(delivery);
+		log.info("Book delivery voucher: {}", voucher.getNumber());
+		buy.setVoucher(voucher.getNumber());
+		buy.setEstimatedDeliveryDate(voucher.getEstimatedDeliveryDate());
+		buy.setStatus(EnumBuyStatus.DELIVERY_BOOKED);
+		buyRepository.save(buy);
 	}
 	
 	public Buy makeBuyFallback(BuyDTO buy) {
-		Buy savedBuy = new Buy();
-		savedBuy.setAdress("Response of fallback on makeBuy");
+		Buy savedBuy = buyRepository.findById(buy.getId())
+						.orElseThrow(() -> 
+							new EmptyResultDataAccessException(String.format("Compra não encontrada para o id %d", buy.getId()), 1));
+		savedBuy.setMessage("Response of fallback on makeBuy");
+		buyRepository.save(savedBuy);
 		return savedBuy;
 	}
 	
 	public Buy getByIdyFallback(Long id) {
 		Buy savedBuy = new Buy();
-		savedBuy.setAdress("Response of fallback on getById");
+		savedBuy.setMessage("Response of fallback on getById");
 		return savedBuy;
 	}
-
 
 }
