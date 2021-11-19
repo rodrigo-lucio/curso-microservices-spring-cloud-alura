1e03050bad7017b888ce2ecd4185632157b19907
Rodrigo Lúcio <luciodigo@gmail.com>
2021-11-18 22:44:54 -0300

    Criado o projeto api gateway zuul

---
+++ api-gateway-zuul/src/test/java/br/com/alura/microservices/apigateway/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
