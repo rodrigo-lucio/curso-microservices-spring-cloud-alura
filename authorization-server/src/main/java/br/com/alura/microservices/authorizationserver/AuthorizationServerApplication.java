304654fa43e8f938d9942fbc28098caba1c0ed68
Rodrigo Lúcio <luciodigo@gmail.com>
2021-12-08 23:46:20 -0300

    Teste de requisição com token

--- authorization-server/src/main/java/br/com/alura/microservices/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/br/com/alura/microservices/authorizationserver/AuthorizationServerApplication.java
@@ -1,17 +1,27 @@
 package br.com.alura.microservices.authorizationserver;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
+@RestController
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
 	}
 
 }
